a869c29691b167f3087bff1e34474ef0483c5a3f
Sawyer Bergeron <sawyerbergeron@gmail.com>
2020-08-12 12:53:45 -0400

    add more algos to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub fn fnaive1(upper: i64) -> i64 {
+pub fn nothing(upper: u64) -> u64 {
+    42
+}
+
+pub fn fnaive1(upper: u64) -> u64 {
     let mut count = 0;
 
     for i in 1..upper {
@@ -13,7 +17,7 @@ pub fn fnaive1(upper: i64) -> i64 {
     count
 }
 
-pub fn fopt1(upper: i64) -> i64 {
+pub fn fopt1(upper: u64) -> u64 {
     let mut factor3 = 0;
     let mut factor5 = 0;
 
@@ -33,7 +37,7 @@ pub fn fopt1(upper: i64) -> i64 {
     count
 }
 
-pub fn fopt2(upper: i64) -> i64 {
+pub fn fopt2(upper: u64) -> u64 {
     let mut factor3 = 0;
     let mut factor5 = 0;
 
@@ -51,4 +55,55 @@ pub fn fopt2(upper: i64) -> i64 {
     }
 
     count
+}
+
+pub fn fopt3(upper: u64) -> u64 {
+    let upper = upper as i64;
+
+    let mut factor3 = 0;
+    let mut factor5 = 0;
+
+    let mut count = 0;
+
+    for i in 1..upper {
+        let first = (i - 3 == factor3) as i64;
+        factor3 += first * 3;
+        count += first;
+        let second = (i - 5 == factor5) as i64;
+        factor5 += second * 5;
+        count += second;
+    }
+
+    count as u64
+}
+
+pub fn fzbz2(up: u64)  -> u64 {
+    let mut cnt = 0;
+    for i in 1..up {
+        cnt += (i % 3 == 0) as u64 + (i % 5 == 0) as u64;
+    } 
+    cnt
+} 
+
+pub fn fzbz1(up: u64) -> u64 {
+    (1..up)
+        .map(|i| {
+            let mut cnt = 0;
+            if i % 3 == 0 {
+                cnt += 1;
+            }
+            if i % 5 == 0 {
+                cnt += 1;
+            }
+            cnt
+        })
+        .sum()
+}
+
+pub fn fbf(u: u64) -> u64 {
+    u/3 + u/5 - (u % 3 == 0) as u64 - (u % 5 == 0) as u64
+}
+
+pub fn test_cast(up: i64) -> u64 {
+    up as u64
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@ pub mod lib;
 use lib::*;
 
 fn main() {
-    let upper = 100000000;
+    let upper = 1000000000;
 
-    for f in [fnaive1, fopt1, fopt2].iter() {
+    for (f, fnname) in [
+        (nothing as fn(u64) -> u64, "empty"),
+        (fnaive1, "fnaive1"),
+        (fopt1, "fopt1"),
+        (fopt2, "fopt2"),
+        (fopt3, "fopt3"),
+        (fzbz2, "fzbz2"),
+        (fzbz1, "fzbz1"),
+        (fbf, "fbf"),
+    ].iter() {
+        //let f: &fn(u64) -> u64 = f;
         let start = Instant::now();
         let result = f(upper);
         let duration = start.elapsed();
 
-        println!("Got result {} with duration {:?}", result, duration);
+        println!("Got result {} with duration {:?} from {}", result, duration, fnname);
     }
 }
